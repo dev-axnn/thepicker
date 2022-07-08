6eeb9105983e6884f6b469e8da57f78fd47e2912
dev-axnn <dev.axnn@gmail.com>
2022-07-08 18:06:52 +0900

    비쥬얼 영역 보충

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-$(document).ready(){};
-
-
-
 window.onload = function(){
   new Swiper('.sw-visual-1', {
+    effect: 'fade',
     loop: true,
+    speed: 1500,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: '.sw-vs-1-pagination',
+      clickable: true,
+    },
   });
 
-}
+};
