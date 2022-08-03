7098a5b134d920ed5e2ddbc671bb3da2454e6325
dev-axnn <dev.axnn@gmail.com>
2022-08-03 22:00:29 +0900

    진행중

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ window.onload = function(){
   new Swiper('.sw-bestproducts', {
     loop: true,
     speed: 1500,
+    slidesPerView : 5, // 동시에 보여줄 슬라이드 갯수
+    spaceBetween : 15, // 슬라이드간 간격
+    slidesPerGroup : 6, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
     autoplay: {
       delay: 7000,
     }
